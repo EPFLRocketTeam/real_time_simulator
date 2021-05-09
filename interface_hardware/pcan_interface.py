6a068e37ddca33a584939812492338ebda2c7d13
iac26 <iacopo.sprenger@epfl.ch>
2021-05-09 18:55:30 +0200

    added support for PEAK pcan interface

---
+++ interface_hardware/pcan_interface.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+import rospy
+import rospkg
+
+import numpy as np
+import math
+import time
+
+from real_time_simulator.msg import Control
+from real_time_simulator.msg import FSM
+from real_time_simulator.msg import State
+from real_time_simulator.msg import Sensor
+
+import can
+import struct
+
+from scipy.interpolate import interp1d
+
+# DATA ID
+DATA_ID_PRESSURE        = 0 
+DATA_ID_ACCELERATION_X  = 1 
+DATA_ID_ACCELERATION_Y  = 2 
+DATA_ID_ACCELERATION_Z  = 3 
+DATA_ID_GYRO_X          = 4 
+DATA_ID_GYRO_Y          = 5 
+DATA_ID_GYRO_Z          = 6 
+
+DATA_ID_GPS_HDOP        = 7 
+DATA_ID_GPS_LAT         = 8 
+DATA_ID_GPS_LONG        = 9 
+DATA_ID_GPS_ALTITUDE    = 10
+DATA_ID_GPS_SATS        = 11
+
+DATA_ID_TEMPERATURE     = 12
+DATA_ID_CALIB_PRESSURE  = 13
+
+DATA_ID_AB_STATE        = 16
+DATA_ID_AB_INC          = 17
+DATA_ID_AB_AIRSPEED     = 18
+DATA_ID_AB_ALT          = 19
+
+DATA_ID_KALMAN_STATE    = 38
+DATA_ID_KALMAN_X        = 40
+DATA_ID_KALMAN_Y        = 41
+DATA_ID_KALMAN_Z        = 42
+DATA_ID_KALMAN_VX       = 43
+DATA_ID_KALMAN_VY       = 44
+DATA_ID_KALMAN_VZ       = 45
+DATA_ID_KALMAN_YAW      = 46
+DATA_ID_KALMAN_PITCH    = 47
+DATA_ID_KALMAN_ROLL     = 48
+DATA_ID_ALTITUDE        = 49
+DATA_ID_COMMAND         = 80
+ 
+DATA_ID_PRESS_1         = 85
+DATA_ID_PRESS_2         = 86
+DATA_ID_TEMP_1          = 87
+DATA_ID_TEMP_2          = 88
+DATA_ID_TEMP_3          = 89
+DATA_ID_STATUS          = 90
+DATA_ID_MOTOR_POS       = 91
+DATA_ID_VANE_POS_1      = 92
+DATA_ID_VANE_POS_2      = 93
+DATA_ID_VANE_POS_3      = 94
+DATA_ID_VANE_POS_4      = 95
+ 
+DATA_ID_TVC_COMMAND     = 100
+DATA_ID_THRUST_CMD      = 101
+DATA_ID_VANE_CMD_1      = 102
+DATA_ID_VANE_CMD_2      = 103
+DATA_ID_VANE_CMD_3      = 104
+DATA_ID_VANE_CMD_4      = 105
+ 
+DATA_ID_TVC_HEARTBEAT   = 106
+
+T2P_CONVERSION = 76.233
+
+
+TVC_CMD_BOOT = 1
+TVC_CMD_SHUTDOWN = 2
+TVC_CMD_ABORT = 3
+
+STATE_IDLE = 0
+STATE_BOOT = 1
+STATE_COMPUTE = 2
+STATE_SHUTDOWN = 3
+STATE_ABORT = 4
+
+
+bus = None
+
+current_fsm = FSM()
+current_control = Control()
+current_kalman = State()
+
+
+def send_message(data_id, data, timestamp=0):
+    b_data = struct.pack(">i", data)
+    b_data_id = struct.pack("b", data_id)
+    b_timestamp = struct.pack(">i", timestamp)
+    send_msg = can.Message(data=b_data+b_data_id+b_timestamp[0:3])
+    bus.send(send_msg) 
+
+
+def recv_message(timeout=0.01):
+    recv_msg = bus.recv(timeout)
+    if(recv_msg is not None and len(recv_msg.data) == 8):
+        data_id = recv_msg.data[4]
+        data = struct.unpack(">i", recv_msg.data[0:4])[0]
+        timestamp = struct.unpack(">i", b'\0'+recv_msg.data[5:8])[0]
+        return {"valid" : True, "id" : data_id, "data" : data, "timestamp" : timestamp}
+    return {"valid" : False, "id" : 0, "data" : 0, "timestamp" : 0}
+
+
+
+def fsm_recv_callback(fsm_data):
+    global current_fsm
+    
+    current_fsm = fsm_data
+
+
+def simu_sensor_callback(simu_sensor):
+    global current_control
+
+    acc_x = round(1000*simu_sensor.IMU_acc.x/9.81)
+    acc_y = round(1000*simu_sensor.IMU_acc.y/9.81)
+    acc_z = round(1000*simu_sensor.IMU_acc.z/9.81)
+
+    gyro_x = round(1000*simu_sensor.IMU_gyro.x/9.81)
+    gyro_y = round(1000*simu_sensor.IMU_gyro.y/9.81)
+    gyro_z = round(1000*simu_sensor.IMU_gyro.z/9.81)
+
+    baro = int(1000*simu_sensor.baro_height)
+
+    cc_press = int(measured_control.force.z*T2P_CONVERSION)
+
+    send_message(DATA_ID_ACCELERATION_X, acc_x)
+    send_message(DATA_ID_ACCELERATION_Y, acc_y)
+    send_message(DATA_ID_ACCELERATION_Z, acc_z)
+
+    send_message(DATA_ID_GYRO_X, gyro_x)
+    send_message(DATA_ID_GYRO_Y, gyro_y)
+    send_message(DATA_ID_GYRO_Z, gyro_z)
+
+    send_message(DATA_ID_ALTITUDE, baro)
+
+    send_message(DATA_ID_PRESS_2, cc_press)
+
+
+
+
+if __name__ == '__main__':
+
+    bus = can.interface.Bus(interface='pcan', channel='PCAN_USBBUS1', bitrate=250000, state=can.bus.BusState.ACTIVE)
+
+    if bus is None:
+        exit(1)
+
+
+    send_message(DATA_ID_TVC_COMMAND, TVC_CMD_BOOT)
+
+    while(1):
+        msg = recv_message()
+        if msg['valid'] and msg['id'] == DATA_ID_TVC_HEARTBEAT and msg['data'] == STATE_COMPUTE:
+            break
+
+    #init ros node
+    rospy.init_node('test_interface', anonymous=True)
+
+    # Publisher for commanded rocket_control 
+    control_pub = rospy.Publisher("control_pub", Control, queue_size=1)
+
+    kalman_pub = rospy.Publisher("kalman_rocket_state", State, queue_size=1)
+
+    # Publisher for measured actuator inputs
+    actuator_pub = rospy.Publisher('control_measured', Control, queue_size=1)
+
+    current_fsm = FSM()
+
+    current_control = Control()
+
+    measured_control = Control()
+
+    # Subscribe to fake sensor from simulation 
+    rospy.Subscriber("simu_sensor_pub", Sensor, simu_sensor_callback)
+
+    rospy.Subscriber("fsm_pub", FSM, fsm_recv_callback)
+
+    rospack = rospkg.RosPack()
+    thrust_curve = np.loadtxt(rospack.get_path('bellalui_gnc') + "/config/motor_file.txt")
+    f_thrust = interp1d(thrust_curve[:,0], thrust_curve[:,1])
+        
+    #variables
+    new_thrust = False
+
+    rate = rospy.Rate(50) #PP heartbeat
+
+    while not rospy.is_shutdown():
+
+        
+
+        #poll can messages
+        while(1):
+            msg = recv_message()
+            if not msg['valid']:
+                break
+            if msg['id'] == DATA_ID_THRUST_CMD:
+                current_control.force.z = msg['data']
+                new_thrust = True
+            if msg['id'] == DATA_ID_KALMAN_X:
+                current_kalman.pose.position.x = msg['data']/1000.0
+            if msg['id'] == DATA_ID_KALMAN_Y:
+                current_kalman.pose.position.y = msg['data']/1000.0
+            if msg['id'] == DATA_ID_KALMAN_Z:
+                current_kalman.pose.position.z = msg['data']/1000.0
+            if msg['id'] == DATA_ID_KALMAN_VX:
+                current_kalman.twist.linear.x = msg['data']/1000.0
+            if msg['id'] == DATA_ID_KALMAN_VY:
+                current_kalman.twist.linear.y = msg['data']/1000.0
+            if msg['id'] == DATA_ID_KALMAN_VZ:
+                current_kalman.twist.linear.z = msg['data']/1000.0
+
+
+        if(new_thrust):
+            new_thrust = False
+            control_pub.publish(current_control)
+
+        kalman_pub.publish(current_kalman)
+
+
+        if current_fsm.time_now > thrust_curve[0,0] and current_fsm.time_now < thrust_curve[-1,0]:
+
+            real_thrust = float(f_thrust(current_fsm.time_now))
+
+            if current_fsm.state_machine != "Idle" and current_fsm.state_machine != "Rail":
+                if current_control.force.z == 0.0:
+                    real_thrust = 0.0
+
+                    
+
+            measured_control.force.z = real_thrust
+            print("real_thrust: ",real_thrust)
+            actuator_pub.publish(measured_control)
+
+
+
+
+        rate.sleep()
+
+        
+    
+
+
+
---
+++ interface_hardware/usb_interface.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+import rospy
+import rospkg
+
+import numpy as np
+import math
+import serial
+import time
+
+from real_time_simulator.msg import Control
+from real_time_simulator.msg import FSM
+from real_time_simulator.msg import State
+from real_time_simulator.msg import Sensor
+
+import msv2
+import struct
+
+from scipy.interpolate import interp1d
+
+#COMMANDS
+GET_STAT = 	0x00
+BOOT = 		0x01
+SHUTDOWN = 	0x02
+DOWNLOAD = 	0x03
+TVC_MOVE = 	0x04
+ABORT = 	0x05
+RECOVER =	0x06
+SENSOR_WRITE =  0x07
+COMMAND_READ =  0x08
+SENSOR_READ =   0x09
+FEEDBACK_WRITE = 0x0A
+
+control_pub = None
+hb = None
+
+CONVERSION = 76.233
+
+
+def fsm_recv_callback(fsm_data):
+    global current_fsm
+    
+    current_fsm = fsm_data
+
+
+def simu_sensor_callback(simu_sensor):
+    global current_control
+
+    
+    acc_x = round(1000*simu_sensor.IMU_acc.x/9.81)
+    acc_y = round(1000*simu_sensor.IMU_acc.y/9.81)
+    acc_z = round(1000*simu_sensor.IMU_acc.z/9.81)
+
+    gyro_x = round(1000*simu_sensor.IMU_gyro.x/9.81)
+    gyro_y = round(1000*simu_sensor.IMU_gyro.y/9.81)
+    gyro_z = round(1000*simu_sensor.IMU_gyro.z/9.81)
+
+    print("sensor received: {}".format(acc_z))
+
+
+    baro = int(1000*simu_sensor.baro_height)
+    
+    sens_data = struct.pack("iii"+"iii"+"i", acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z, baro)
+
+    resp = hb.send(SENSOR_WRITE,  sens_data)
+    
+    resp = hb.send(COMMAND_READ,   sens_data)
+
+
+    if(resp and len(resp) == 46):
+        cmd_data = struct.unpack("i" + "iiii"+"iii"+"iii"+"H", bytes(resp))
+
+        current_kalman = State()
+        if current_fsm.state_machine == "Idle":
+            current_control.force.z = 2000
+        else:
+            current_control.force.z = cmd_data[0]
+
+        print("current_Control: {}".format(cmd_data[0]))
+
+        control_pub.publish(current_control)
+
+        current_kalman.pose.position.x = cmd_data[5]/1000.0
+        current_kalman.pose.position.y = cmd_data[6]/1000.0
+        current_kalman.pose.position.z = cmd_data[7]/1000.0
+
+        current_kalman.twist.linear.x = cmd_data[8]/1000.0
+        current_kalman.twist.linear.y = cmd_data[9]/1000.0
+        current_kalman.twist.linear.z = cmd_data[10]/1000.0
+
+        current_kalman.pose.orientation.w = 1.0
+
+        
+        kalman_pub.publish(current_kalman)
+
+
+    bin_data =  struct.pack("I", int(measured_control.force.z*CONVERSION))
+
+    resp = hb.send(FEEDBACK_WRITE, bin_data)
+
+
+if __name__ == '__main__':
+
+    hb = msv2.msv2()
+
+    if hb.connect("/dev/ttyACM0"):
+        print("connected")
+    else:
+        print("error")
+
+    state_Pi = 1
+
+    while state_Pi == 1:
+        time.sleep(1)
+
+        stat = hb.send(GET_STAT, [0x00, 0x00])
+        if stat == -1 or stat==0:
+            continue
+
+        if(stat and len(stat) == 20):
+            data = struct.unpack("HHiIiHBb", bytes(stat))
+            state_Pi = data[0]
+    
+    
+    hb.send(BOOT, [0x00, 0x00])
+
+    print("initializing ros...")
+    # Init ROS
+    rospy.init_node('test_interface', anonymous=True)
+    
+    # Publisher for commanded rocket_control 
+    control_pub = rospy.Publisher("control_pub", Control, queue_size=1)
+
+    kalman_pub = rospy.Publisher("kalman_rocket_state", State, queue_size=1)
+
+    # Publisher for measured actuator inputs
+    actuator_pub = rospy.Publisher('control_measured', Control, queue_size=1)
+
+    current_fsm = FSM()
+
+    current_control = Control()
+
+    measured_control = Control()
+
+    # Subscribe to fake sensor from simulation 
+    rospy.Subscriber("simu_sensor_pub", Sensor, simu_sensor_callback)
+
+    rospy.Subscriber("fsm_pub", FSM, fsm_recv_callback)
+
+    if rospy.get_param("/simulation") == 3:
+
+
+        # Load motor thrust curve to get real thrust (for control_measured)
+        rospack = rospkg.RosPack()
+        thrust_curve = np.loadtxt(rospack.get_path('bellalui_gnc') + "/config/motor_file.txt")
+        f_thrust = interp1d(thrust_curve[:,0], thrust_curve[:,1])
+            
+        rate = rospy.Rate(100)
+
+        while not rospy.is_shutdown():
+        
+            # Thread sleep time defined by rate
+            rate.sleep()
+            
+            if current_fsm.time_now > thrust_curve[0,0] and current_fsm.time_now < thrust_curve[-1,0]:
+
+                real_thrust = float(f_thrust(current_fsm.time_now))
+
+                if current_fsm.state_machine != "Idle" and current_fsm.state_machine != "Rail":
+                    if current_control.force.z == 0.0:
+                        real_thrust = 0.0
+
+                        
+
+                measured_control.force.z = real_thrust
+                print("real_thrust: ",real_thrust)
+                actuator_pub.publish(measured_control)
+
+    #rospy.spin()
+
