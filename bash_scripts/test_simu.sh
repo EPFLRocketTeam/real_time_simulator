dc95102ca250be80f8f7bfa9a5e855ddc7199140
AlbericLaj <albericlajarte@gmail.com>
2021-11-13 17:58:06 +0100

    corrected test_simu script

--- bash_scripts/test_simu.sh
+++ bash_scripts/test_simu.sh
@@ -8,7 +8,7 @@ cd ../../
 catkin_make
 source devel/setup.bash
 
-(sleep 5;rostopic pub /control_pub real_time_simulator/Control "torque:
+(sleep 5;rostopic pub /control_measured real_time_simulator/Control "torque:
   x: 0.0
   y: 0.0
   z: 0.0
